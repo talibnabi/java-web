a311521fd824c76a1bddf489c5221a0bdfffa221
talibnabi <talibnabiyev@gmail.com>
2022-09-29 04:44:46 -0700

    commit

--- lesson-22/src/main/java/lesson/StaticFileServlet.java
+++ lesson-22/src/main/java/lesson/StaticFileServlet.java
@@ -14,10 +14,13 @@ public class StaticFileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String pathInfo = req.getPathInfo();
+        if (pathInfo.startsWith("/")) pathInfo = pathInfo.substring(1);
+        String fileName = getClass().getClassLoader().getResource(pathInfo).getFile();
         System.out.println(pathInfo);
+        System.out.println(fileName);
 //        String bestExample = getClass().getClassLoader().getResource("college.jpg").getFile();
         try (ServletOutputStream os = resp.getOutputStream()) {
-            Files.copy(Path.of(pathInfo), os);
+            Files.copy(Path.of(fileName), os);
         }
     }
 }
