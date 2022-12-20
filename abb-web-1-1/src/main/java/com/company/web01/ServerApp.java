608ba53f294d6e7693060f8dab5ce44e2a454812
talibnabi <talibnabiyev@gmail.com>
2022-12-20 09:52:39 -0800

    commit

---
+++ abb-web-1-1/src/main/java/com/company/web01/ServerApp.java
@@ -0,0 +1,17 @@
+package com.company.web01;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class ServerApp {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8099);
+
+        ServletContextHandler handler = new ServletContextHandler();
+        handler.addServlet(HelloServlet.class, "/hello");
+
+        server.setHandler(handler);
+        server.start();
+        server.join();
+    }
+}
